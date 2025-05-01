ddbd9120916c29956c2b1c406446a96a90695134
Wesley Schuab <yinnihisoka@gmail.com>
2025-05-01 20:45:44 -0300

    Reformatando o codigo

--- app/src/main/java/com/example/basicodekotlin/Enum.kt
+++ app/src/main/java/com/example/basicodekotlin/Enum.kt
@@ -3,7 +3,7 @@ package com.example.basicodekotlin
 //Aguardando aprovação
 //realizado
 //Pagamento_confirmado
-enum class StatusPedido{
+enum class StatusPedido {
     AGURDANDO_APROVACAO,
     PEDIDO_REALIZADO,
     PAGAMENTO_CONFIRMADO,
@@ -11,26 +11,28 @@ enum class StatusPedido{
     PEDIDO_ENTREGUE
 
 }
-class Pedido( var total: Double = 0.0,
-              var intens : String = "",
-              var statusPedido : StatusPedido = StatusPedido.AGURDANDO_APROVACAO
-){
+
+class Pedido(
+    var total: Double = 0.0,
+    var intens: String = "",
+    var statusPedido: StatusPedido = StatusPedido.AGURDANDO_APROVACAO
+) {
 
 
 }
 
-fun main(){
+fun main() {
     // Mudando para ordinal
     println("Status do Pedido: ${StatusPedido.AGURDANDO_APROVACAO.ordinal}")
     // Tela de compras
-val pedido = Pedido(125.9,"Camiseta, livro")
+    val pedido = Pedido(125.9, "Camiseta, livro")
     // pagamento com cartão
     pedido.statusPedido = StatusPedido.PEDIDO_REALIZADO
 
     // Historico de compra
-    if (pedido.statusPedido == StatusPedido.PEDIDO_REALIZADO){
+    if (pedido.statusPedido == StatusPedido.PEDIDO_REALIZADO) {
         println("O seu pedido foi realizado")
-    } else if (pedido.statusPedido == StatusPedido.PAGAMENTO_CONFIRMADO){
+    } else if (pedido.statusPedido == StatusPedido.PAGAMENTO_CONFIRMADO) {
         println("O seu pagamento foi confirmado")
     }
 }
