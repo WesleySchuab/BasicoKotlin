10982f35a8c3df44d518f072cfd6d9863902d33c
Wesley Schuab <yinnihisoka@gmail.com>
2025-05-03 19:06:41 -0300

    Passando o layout conversas para o metodo onCreateView

--- fragments/src/main/java/com/example/fragments/fragments/ConversarFraments.kt
+++ fragments/src/main/java/com/example/fragments/fragments/ConversarFraments.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fragments.R
 
 class ConversarFraments : Fragment () {
     // Metodo que vai construir a visualização
@@ -13,6 +14,8 @@ class ConversarFraments : Fragment () {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(
+            R.layout.fragments_conversas, container, false
+        )
     }
 }
