3757bda1b87923ddd869ae157c299a8a626be898
Wesley Schuab <yinnihisoka@gmail.com>
2025-05-04 17:42:25 -0300

    Interagindo com dados dentro do fragment

--- fragments/src/main/java/com/example/fragments/fragments/ConversarFraments.kt
+++ fragments/src/main/java/com/example/fragments/fragments/ConversarFraments.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.fragments.R
 
-class ConversarFraments : Fragment (R.layout.fragments_conversas) {
+class ConversarFraments : Fragment() {
+    private lateinit var btnExecutar: Button
+    private lateinit var ediNome: EditText
+    private lateinit var textNome: TextView
+
+    //class ConversarFraments : Fragment (R.layout.fragments_conversas) {
     // Metodo que vai construir a visualização
-    /*override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,6 +25,14 @@ class ConversarFraments : Fragment (R.layout.fragments_conversas) {
         val view = inflater.inflate(
             R.layout.fragments_conversas, container, false
         )
+        // Processamento da visualização
+        btnExecutar = view.findViewById(R.id.btnExecutar)
+        ediNome = view.findViewById(R.id.editNome)
+        textNome = view.findViewById(R.id.textResultado)
+
+        btnExecutar.setOnClickListener {
+            textNome.text = ediNome.text.toString()
+        }
         return view
-    }*/
+    }
 }
